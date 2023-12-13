a48f9b21d64a3b2479c6a23485bf2e1f08b86818
steveuniverseuwu <153719514+steveuniverseuwu@users.noreply.github.com>
2023-12-13 17:00:46 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const circle = document.querySelector('.circle img');
+
+    circle.addEventListener('mouseover', () => {
+        circle.style.transform = 'scale(1.2)';
+    });
+
+    circle.addEventListener('mouseout', () => {
+        circle.style.transform = 'scale(1)';
+    });
+});
+document.addEventListener('DOMContentLoaded', function () {
+    const circle = document.querySelector('.circle2 img');
+
+    circle.addEventListener('mouseover', () => {
+        circle.style.transform = 'scale(1.2)';
+    });
+
+    circle.addEventListener('mouseout', () => {
+        circle.style.transform = 'scale(1)';
+    });
+});
+
